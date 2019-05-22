109ec201e058d686e2aeb880d26e76112d8227db
mdkarim86 <mdkarim86@users.noreply.github.com>
2019-05-22 12:51:31 -0400

    practice project

---
+++ pp/src/test/java/PP/pp/Numseries.java
@@ -0,0 +1,15 @@
+package PP.pp;
+
+public class Numseries {
+
+	public static void main(String[] args) {
+		String number="1234567890";
+		String num="";
+		for(int i=0; i<number.length(); i++) {
+			num=num+number.charAt(i);
+			System.out.println(num);
+		}
+
+	}
+
+}
